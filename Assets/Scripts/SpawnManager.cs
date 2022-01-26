5c56137098f358f7bd300bb013af29083ad5cbbd
MJ <mjlinane@oldrochester.org>
2022-01-26 15:32:55 -0500

    Update SpawnManager.cs

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -3,7 +3,7 @@
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
-    private float spawnRangeX = 20;
+    private float spawnRangeX = 20.0f;
     private float spawnPosZ = 25;
     private float startDelay = 2;
     private float spawnInterval = 1.5f;
