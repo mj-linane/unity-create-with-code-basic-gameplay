8935d854184635460f01ae87fd3306c528880282
MJ <mjlinane@oldrochester.org>
2022-05-23 10:56:31 -0400

    Update PlayerControllerWithProjectile.cs

--- Assets/Scripts/PlayerControllerWithProjectile.cs
+++ Assets/Scripts/PlayerControllerWithProjectile.cs
@@ -2,31 +2,28 @@
 
 public class PlayerControllerWithProjectile : MonoBehaviour
 {
-  private float horizontalInput;
-  private float speed = 10.0f;
-  private float xRange = 20;
-  public GameObject projectilePrefab;
+    private float horizontalInput;
+    private float speed = 10.0f;
+    private float xRange = 20;
+    public GameObject projectilePrefab;
 
-  private Vector3 offset = new Vector3(0, 1.5f, 0);
+    private Vector3 offset = new(0, 1.5f, 0);
 
-  // Update is called once per frame
-  void Update()
-  {
-    horizontalInput = Input.GetAxis("Horizontal");
-    transform.Translate(Vector3.right * (horizontalInput * Time.deltaTime * speed));
-    if (transform.position.x < -xRange)
+    // Update is called once per frame
+    private void Update()
     {
-      transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
-    }
-    if (transform.position.x > xRange)
-    {
-      transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
-    }
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * (horizontalInput * Time.deltaTime * speed));
+        if (transform.position.x < -xRange)
+            transform.position = new Vector3(
+                -xRange, transform.position.y, transform.position.z
+            );
 
-    if (Input.GetKeyDown(KeyCode.Space))
-    {
-      // Launch food from player
-      Instantiate(projectilePrefab, transform.position + offset, projectilePrefab.transform.rotation);
+        if (transform.position.x > xRange)
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            // Launch food from player
+            Instantiate(projectilePrefab, transform.position + offset, projectilePrefab.transform.rotation);
     }
-  }
 }
